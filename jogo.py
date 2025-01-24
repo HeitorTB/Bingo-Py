b278acba70c9c230a07f49dd2fe18fc8515b2b73
Heitor Brasil <heitortbr@gmail.com>
2025-01-24 10:31:01 -0300

    Primeira edição
    Criação dos niveis e iniciamento do jogo

---
+++ jogo.py
@@ -0,0 +1,22 @@
+# Configurações dos níveis
+niveis = {
+    0: {"players": 2, "linhas": 2, "colunas":3},
+    1: {"players": 4, "linhas": 3, "colunas":4},
+}
+
+def jogo(nivel):
+    config = niveis[nivel]
+    Sorted=[]
+
+    for c in range (config["players"]):
+
+        
+
+
+
+
+
+
+
+
+
