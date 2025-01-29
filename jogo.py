f580e6fa9d338bf29ca647918ad7fd58b6a20031
Heitor Brasil <heitortbr@gmail.com>
2025-01-29 10:29:40 -0300

    Sorteamento de numero
    tentativa de criar um looping que sorteie o num

--- jogo.py
+++ jogo.py
@@ -7,13 +7,26 @@ niveis = {
 def exibir_matriz(matriz):
     for linha in matriz:
         print('  '.join(str(num) for num in linha))
-        
+
 intervalos = ((1, 10), (11, 20), (21, 30), (31, 40))
-numeros_sorteados1=[]
+
+
+def verificação_dezena(nivel, matriz, niveis):
+    colunas = niveis[nivel]["colunas"]
+    num_sorteado = random.randint(1, colunas * 10)
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == num_sorteado:
+                matriz[i][j] = f'({matriz[i][j]})' 
+    return matriz 
+        
+                    
+
+
 
 def criar_cartekas(nivel):
-    matriz=[]
-    usados=[]
+    matriz = []
+    usados = []
     for i in range(niveis[nivel]["linhas"]):
         linha = []
         for c in range(niveis[nivel]["colunas"]):
@@ -22,17 +35,23 @@ def criar_cartekas(nivel):
             while num in usados:
                 num = random.randint(intervalo[0], intervalo[1])
             usados.append(num)
-            linha.append(num)
-        matriz.append(linha)
-    return exibir_matriz(matriz)
+            if num < 10:
+                linha.append(str(num).zfill(2))
+            else:
+                linha.append(num)
+        matriz.append(linha)  
+    exibir_matriz(matriz) 
 
 def jogo(nivel):
     config = niveis[nivel]
     for c in range(1, config["players"] + 1):
         print(f"Jogador {c}")
-        print(criar_cartekas(nivel))
+        criar_cartekas(nivel)
+        verificação_dezena(nivel,matriz=exibir_matriz,niveis=len(exibir_matriz))
+        
 
-jogo(1)
+
+jogo(0)
 
 
 
